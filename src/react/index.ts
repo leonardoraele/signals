86bbb14955973fb0172bbd5ba58f4c17e08422f2
Leonardo Raele <leonardoraele@gmail.com>
2026-09-18 06:33:14 -0300

    ref(minor): Removing unused import

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { SignalComputed } from '../SignalComputed.js';
 import { SignalEffect } from '../SignalEffect.js';
 import { SignalState } from '../SignalState.js';
